4e4abb9f59d24e4c5cd5b7aa2e128110e5c5dfa7
Michael Schumann <schumann82@gmail.com>
2018-06-13 14:02:51 -0700

    starter code

---
+++ assets/javascript/train.js
@@ -0,0 +1,17 @@
+
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyBoHxMoxZGMttljXvEhZtDMuMy4EiHp_jA",
+authDomain: "classapp-fa23d.firebaseapp.com",
+databaseURL: "https://classapp-fa23d.firebaseio.com",
+projectId: "classapp-fa23d",
+storageBucket: "",
+messagingSenderId: "1042086065264"
+};
+
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+
