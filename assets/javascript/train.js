cc4536aef066f1b271ede156dffd2144c5cf682f
Michael Schumann <schumann82@gmail.com>
2018-06-13 15:36:38 -0700

    push to database

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,4 +1,4 @@
-
+$(document).ready(function() {
 // Initialize Firebase
 var config = {
 apiKey: "AIzaSyBoHxMoxZGMttljXvEhZtDMuMy4EiHp_jA",
@@ -14,4 +14,37 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
+
+var trainName = "";
+var destination = "";
+var frequency = 0;
+var nextArrival = "";
+var minutesAway = 0;
+var firstTrain = "";
+
+$("#add-train").on("click", function(event) {
+    // Don't refresh the page!
+    event.preventDefault();
+
+    
+    trainName = $("#name-input").val().trim();
+    destination = $("#destination-input").val().trim();
+    frequency = $("#frequency-input").val().trim();
+    firstTrain = $("#first-input").val().trim();
+
+    database.ref().push({
+      trainName: trainName,
+      destination: destination,
+      frequency: frequency,
+      firstTrain: firstTrain, // first part is child of database. after : is the value.
+    });
+
+    console.log(trainName);
+    console.log(destination);
+    console.log(frequency);
+    console.log(firstTrain);
+
+  });
+
+});
 
